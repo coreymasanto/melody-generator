94ab60c926e8a5d6d4dd2b39f42396cd7e996aa1
CMasanto <coreymasanto@gmail.com>
2015-12-15 10:47:01 -0500

    Use third-order chain to determine next note, followed by second-order (if third-order fails), and finally first-order

--- MelodyGenerator.py
+++ MelodyGenerator.py
@@ -2,36 +2,44 @@ from collections import defaultdict
 from midiutil.MidiFile import MIDIFile
 import nltk
 from nltk.corpus import brown
+from nltk.util import ngrams
 import random
 import re
 import sys
 
 def main():
         # Open training file
-	print("Reading corpus files...")
+	print("Reading notes from corpus files...")
 	notesFilePath = "corpus/MichaelJacksonNotes.txt"
 	notesFile = open(notesFilePath, 'r')
-	
         notes = getTokensFromFile(notesFile)
-        print(notes)
 
-        print("Creating transition matrix...")
-        transitionMatrix = getTransitionMatrix(notes)
+        print("Creating unary transition matrix...")
+        unaryTransitionMatrix = getUnaryTransitionMatrix(notes)
+
+        print("Creating binary transition matrix...")
+        binaryTransitionMatrix = getBinaryTransitionMatrix(notes)
+
+        print("Creating ternary transition matrix...")
+        ternaryTransitionMatrix = getTernaryTransitionMatrix(notes)
 
         print("Generating verse notes...")
-        verseFirstNote = 61
-        verseSecondNote = 61
-        verseList = generateVerse(transitionMatrix, verseFirstNote, verseSecondNote, notes)
+        vn1 = 61
+        vn2 = 61
+        vn3 = 61
+        verseList = generateVerse(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, vn1, vn2, vn3)
 
         print("Generating chorus notes...")
-        chorusFirstNote = 57
-        chorusSecondNote = 56
-        chorusList = generateChorus(transitionMatrix, chorusFirstNote, chorusSecondNote, notes)
+        cn1 = 61
+        cn2 = 56
+        cn3 = 68
+        chorusList = generateChorus(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, cn1, cn2, cn3)
 
         print("Generating bridge notes...")
-        bridgeFirstNote = 68
-        bridgeSecondNote = 69
-        bridgeList = generateBridge(transitionMatrix, bridgeFirstNote, bridgeSecondNote, notes)
+        bn1 = 57
+        bn2 = 69
+        bn3 = 61
+        bridgeList = generateBridge(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, bn1, bn2, bn3)
 
         print("Creating MIDI file...")
         songList = getSongList(verseList, chorusList, bridgeList)
@@ -46,14 +54,16 @@ def main():
         # Add track name and tempo.
         song.addTrackName(track, time, "Generated Song")
         song.addTempo(track, time, 240)
+        
         for note in songList:
                 track = 0
                 channel = 0
                 duration = random.randint(1, 4)
-                if note >= 0:
+                noteIsSilent = note == -1
+                if not noteIsSilent:
                         pitch = note
                         volume = 100
-                else:  # Values of -1 (i.e., rests)
+                else:
                         pitch = 0
                         volume = 0
                 song.addNote(track, channel, pitch, time, duration, volume)
@@ -79,25 +89,45 @@ def getTokensFromFile(notesFile):
 
 
 
-def getTransitionMatrix(tokens):
-        transitionMatrix = {}
-        for (t1, t2, t3) in nltk.trigrams(tokens):
-                if (t1, t2) not in transitionMatrix:
-                        transitionMatrix[(t1, t2)] = [] 
-                transitionMatrix[(t1, t2)].append(t3)
-        return transitionMatrix
+def getUnaryTransitionMatrix(tokens):
+        unaryTransitionMatrix = {}
+        for (t1, t2) in ngrams(tokens, 2):
+                if t1 not in unaryTransitionMatrix:
+                        unaryTransitionMatrix[t1] = [] 
+                unaryTransitionMatrix[t1].append(t2)
+        return unaryTransitionMatrix
+
+
+def getBinaryTransitionMatrix(tokens):
+        binaryTransitionMatrix = {}
+        for (t1, t2, t3) in ngrams(tokens, 3):
+                if (t1, t2) not in binaryTransitionMatrix:
+                        binaryTransitionMatrix[(t1, t2)] = [] 
+                binaryTransitionMatrix[(t1, t2)].append(t3)
+        return binaryTransitionMatrix
+
+
+
+def getTernaryTransitionMatrix(tokens):
+        ternaryTransitionMatrix = {}
+        for (t1, t2, t3, t4) in ngrams(tokens, 4):
+                if (t1, t2, t3) not in ternaryTransitionMatrix:
+                        ternaryTransitionMatrix[(t1, t2, t3)] = [] 
+                ternaryTransitionMatrix[(t1, t2, t3)].append(t4)
+        return ternaryTransitionMatrix
 
 
 
 # Generate 4 groups of 8 notes, each separated by a half-rest
-def generateVerse(transitionMatrix, firstNote, secondNote, tokens):
-        verse = [firstNote, secondNote]
+def generateVerse(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote):
+        verse = [firstNote, secondNote, thirdNote]
         for line in range(4):
                 for note in range(8):
-                        bestNote = nextNote(transitionMatrix, firstNote, secondNote, tokens)
+                        bestNote = nextNote(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote)
                         verse.append(bestNote)
                         firstNote = secondNote
-                        secondNote = bestNote
+                        secondNote = thirdNote
+                        thirdNote = bestNote
                 for rest in range(2):
                         verse.append(-1)
         return verse
@@ -105,38 +135,42 @@ def generateVerse(transitionMatrix, firstNote, secondNote, tokens):
 
         
 # Generate 4 groups of 8 notes, each separated by a half-rest
-def generateChorus(transitionMatrix, firstNote, secondNote, tokens):
-        chorus = [firstNote, secondNote]
+def generateChorus(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote):
+        chorus = [firstNote, secondNote, thirdNote]
         for line in range(4):
                 for note in range(8):
-                        bestNote = nextNote(transitionMatrix, firstNote, secondNote, tokens)
+                        bestNote = nextNote(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote)
                         chorus.append(bestNote)
                         firstNote = secondNote
-                        secondNote = bestNote
+                        secondNote = thirdNote
+                        thirdNote = bestNote
                 for rest in range(2):
                         chorus.append(-1)
         return chorus
 
 
 
-def generateBridge(transitionMatrix, firstNote, secondNote, tokens):
-        bridge = [firstNote, secondNote]
+def generateBridge(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote):
+        bridge = [firstNote, secondNote, thirdNote]
         for line in range(4):
                 for note in range(8):
-                        bestNote = nextNote(transitionMatrix, firstNote, secondNote, tokens)
+                        bestNote = nextNote(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, firstNote, secondNote, thirdNote)
                         bridge.append(bestNote)
                         firstNote = secondNote
-                        secondNote = bestNote
+                        secondNote = thirdNote
+                        thirdNote = bestNote
         return bridge
 
 
 
-def nextNote(transitionMatrix, n1, n2, notes):
-        if (n1, n2) in transitionMatrix.keys():
-                possibleNotes = transitionMatrix[(n1, n2)]
+def nextNote(unaryTransitionMatrix, binaryTransitionMatrix, ternaryTransitionMatrix, n1, n2, n3):
+        if (n1, n2, n3) in ternaryTransitionMatrix.keys():
+                possibleNotes = ternaryTransitionMatrix[(n1, n2, n3)]
+        elif (n2, n3) in binaryTransitionMatrix:
+                possibleNotes = binaryTransitionMatrix[(n2, n3)]
         else:
-                possibleNotes = []
-        return random.choice(possibleNotes) if len(possibleNotes) > 0 else random.choice(notes)
+                possibleNotes = unaryTransitionMatrix[n3]
+        return random.choice(possibleNotes)
 
 
 
